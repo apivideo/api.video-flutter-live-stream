5ad3ab63208792154d955b8684f9e0a6d0991479
ThibaultBee <thibault@api.video>
2022-01-13 10:42:51 +0100

    fix(lib): fix flutter analyze warnings

--- lib/apivideo_live_stream.dart
+++ lib/apivideo_live_stream.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 import 'src/types/audio_parameters.dart';
