d02e9e492a7629b97ff8779c7700bfee2fceef03
ThibaultBee <thibault@api.video>
2022-01-21 11:06:14 +0100

    chore(test): Add minimalist test on startStreaming

--- test/apivideo_live_stream_test.dart
+++ test/apivideo_live_stream_test.dart
@@ -1,18 +1,32 @@
+import 'package:apivideo_live_stream/apivideo_live_stream.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('apivideolivestream');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
+  const MethodChannel channel = MethodChannel("video.api.livestream/controller");
+  final _controller = LiveStreamController();
+
+  test('startStreaming', () async {
+    final url = "rtmp://test";
+    final streamKey = "abcde";
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      expect(methodCall.method, "startStreaming");
+      expect(methodCall.arguments["url"], url);
+      expect(methodCall.arguments["streamKey"], streamKey);
+      return;
     });
+    _controller.startStreaming(streamKey: streamKey, url: url);
+    channel.setMockMethodCallHandler(null);
   });
 
-  tearDown(() {
+  test('startStreaming with exception', () async {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      throw Exception();
+    });
+
+    expect(_controller.startStreaming(streamKey: "abcde"), throwsException);
     channel.setMockMethodCallHandler(null);
   });
 }
