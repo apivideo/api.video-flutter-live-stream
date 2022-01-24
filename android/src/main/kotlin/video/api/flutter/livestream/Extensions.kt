f1d0f51282634754a7ecba6505e4eccff10edb4c
ThibaultBee <thibault@api.video>
2022-01-24 16:43:45 +0100

    refactor(android): move String to Resolution convertion to Extensions.kt

--- android/src/main/kotlin/video/api/flutter/livestream/Extensions.kt
+++ android/src/main/kotlin/video/api/flutter/livestream/Extensions.kt
@@ -1,12 +1,13 @@
 package video.api.flutter.livestream
 
+import video.api.livestream.enums.Resolution
 import video.api.livestream.models.AudioConfig
 import video.api.livestream.models.VideoConfig
 
 fun Map<String, Any>.toVideoConfig(): VideoConfig {
     return VideoConfig(
         bitrate = this["bitrate"] as Int,
-        resolution = ConfigHelper.getResolutionFromResolutionString(this["resolution"] as String),
+        resolution = (this["resolution"] as String).toResolution(),
         fps = this["fps"] as Int
     )
 }
@@ -19,6 +20,18 @@ fun Map<String, Any>.toAudioConfig(): AudioConfig {
         noiseSuppressor = this["enableNoiseSuppressor"] as Boolean,
         echoCanceler = this["enableEchoCanceler"] as Boolean
     )
+}
+
+fun String.toResolution(): Resolution {
+    return when (this) {
+        "240p" -> Resolution.RESOLUTION_240
+        "360p" -> Resolution.RESOLUTION_360
+        "480p" -> Resolution.RESOLUTION_480
+        "720p" -> Resolution.RESOLUTION_720
+        "1080p" -> Resolution.RESOLUTION_1080
+        "2160p" -> Resolution.RESOLUTION_2160
+        else -> Resolution.RESOLUTION_720
+    }
 }
 
 
